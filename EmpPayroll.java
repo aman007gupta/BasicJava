68b7b4ccbdc2e51622cf5fd9dd362731b5bda69c
aman007gupta <aman9453122969@gmail.com>
2022-04-06 12:31:51 +0530

    Calculate Emp wage using switch

--- EmpPayroll.java
+++ EmpPayroll.java
@@ -1,22 +1,23 @@
 public class EmpPayroll {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int WAGE_PER_HOUR = 100;
+
 	public static void main(String args[]) {
 		System.out.println("Welcome to Emp Payroll");
-		//Contant variable in capital latter
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int WAGE_PER_HOUR = 100;
-		//Variables
 		int empHrs = 0;
 		int totalWage = 0;
-		double empCheck = Math.floor(Math.random()*10)%3;
-		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Emp is permanent");
+		int empCheck = (int) Math.floor(Math.random()*10)%3;
+		switch (empCheck) {
+			case IS_FULL_TIME:
 			empHrs = 8;
-		} else if (empCheck == IS_PART_TIME) {
-			System.out.println("Emp is part time");
+			break;
+			case IS_PART_TIME:
 			empHrs = 4;
-		} else 
-			System.out.println("Emp is absent");
+			break;
+			default:
+			empHrs = 0;
+		}
 		totalWage = empHrs * WAGE_PER_HOUR;
 		System.out.println("Total wage of emp: " + totalWage);
 	}

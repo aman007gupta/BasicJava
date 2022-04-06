499e1ce9b4521a9a3eb0af777ce2a93f88c509e3
aman007gupta <aman9453122969@gmail.com>
2022-04-06 10:19:06 +0530

    Calculate total Emp wage for Emp

--- EmpPayroll.java
+++ EmpPayroll.java
@@ -3,10 +3,19 @@ public class EmpPayroll {
 		System.out.println("Welcome to Emp Payroll");
 		//Contant variable in capital latter
 		int IS_FULL_TIME = 1;
+		int WAGE_PER_HOUR = 100;
+		//Variables
+		int empHrs = 0;
+		int totalWage = 0;
 		double empCheck = Math.floor(Math.random()*10)%2;
-		if (empCheck == IS_FULL_TIME)  //for single statement no need of brackets
+		if (empCheck == IS_FULL_TIME) {  //for single statement no need of brackets
 			System.out.println("Emp is present");
-		else
+			empHrs = 8;
+		} else {
 			System.out.println("Emp is absent");
+			empHrs = 0;
+		}
+		totalWage = empHrs * WAGE_PER_HOUR;
+		System.out.println("Total wage of emp: " + totalWage);
 	}
 }

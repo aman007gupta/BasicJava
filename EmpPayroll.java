a8d9f1a912ebf2f85c012f41269a35ce1d6197f1
aman007gupta <aman9453122969@gmail.com>
2022-04-06 14:28:32 +0530

    Calculate wage based on working days or working hrs reached

--- EmpPayroll.java
+++ EmpPayroll.java
@@ -3,13 +3,17 @@ public class EmpPayroll {
 	public static final int IS_PART_TIME = 2;
 	public static final int WAGE_PER_HOUR = 100;
 	public static final int NUM_OF_DAYS = 20;
+	public static final int TOTAL_HRS = 50;
 
 	public static void main(String args[]) {
 		System.out.println("Welcome to Emp Payroll");
 		int empHrs = 0;
+		int totalHrs = 0;
 		int totalWage = 0;
 		int totalEmpWage = 0;
-		for (int days = 0; days < NUM_OF_DAYS; days++) {
+		int totalDays = 0;
+		while (totalHrs < TOTAL_HRS && totalDays < NUM_OF_DAYS) {
+			totalDays++;
 			int empCheck = (int) Math.floor(Math.random()*10)%3;
 			switch (empCheck) {
 				case IS_FULL_TIME:
@@ -21,9 +25,11 @@ public class EmpPayroll {
 				default:
 					empHrs = 0;
 			}
-		totalWage = empHrs * WAGE_PER_HOUR;
-		totalEmpWage += totalWage;
+			totalHrs += empHrs; 
+			totalWage = totalHrs * WAGE_PER_HOUR;
+			totalEmpWage += totalWage;
 		}
 		System.out.println("Total wage of emp: " + totalEmpWage);
+		System.out.println("Total Days: " + totalDays + " Total Hrs: " + totalHrs);
 	}
 }

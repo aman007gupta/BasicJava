87bbc0662588ff1f86403ae2eb5ece477226483e
aman007gupta <aman9453122969@gmail.com>
2022-04-06 10:33:19 +0530

    Add part time Emp

--- EmpPayroll.java
+++ EmpPayroll.java
@@ -3,18 +3,20 @@ public class EmpPayroll {
 		System.out.println("Welcome to Emp Payroll");
 		//Contant variable in capital latter
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int WAGE_PER_HOUR = 100;
 		//Variables
 		int empHrs = 0;
 		int totalWage = 0;
-		double empCheck = Math.floor(Math.random()*10)%2;
-		if (empCheck == IS_FULL_TIME) {  //for single statement no need of brackets
-			System.out.println("Emp is present");
+		double empCheck = Math.floor(Math.random()*10)%3;
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Emp is permanent");
 			empHrs = 8;
-		} else {
+		} else if (empCheck == IS_PART_TIME) {
+			System.out.println("Emp is part time");
+			empHrs = 4;
+		} else 
 			System.out.println("Emp is absent");
-			empHrs = 0;
-		}
 		totalWage = empHrs * WAGE_PER_HOUR;
 		System.out.println("Total wage of emp: " + totalWage);
 	}

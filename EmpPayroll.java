d6fc3e92e24d4c1a1670e106e70b97cfc2ca234e
aman007gupta <aman9453122969@gmail.com>
2022-04-06 09:59:23 +0530

    Check emp is present or not

--- EmpPayroll.java
+++ EmpPayroll.java
@@ -1,5 +1,12 @@
 public class EmpPayroll {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Emp Payroll");
+		//Contant variable in capital latter
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_TIME)  //for single statement no need of brackets
+			System.out.println("Emp is present");
+		else
+			System.out.println("Emp is absent");
 	}
 }
